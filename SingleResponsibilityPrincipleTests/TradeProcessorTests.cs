a1e95f22168491dd201e0cec774950a202ee277f
Alex Zenk <azenk@css.edu>
2018-10-25 11:41:19 -0500

    commit

---
+++ SingleResponsibilityPrincipleTests/TradeProcessorTests.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SingleResponsibilityPrinciple;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingleResponsibilityPrinciple.Tests
+{
+    [TestClass()]
+    public class TradeProcessorTests
+    {
+        [TestMethod()]
+
+        public void TestNormalFile()
+        {
+            //Arrange
+            var tradeStream = Assembly.GetExecutingAssembly().GetManifestResourceStream("SingleResponsibilityPrincipleTests.goodtrades.txt");
+
+            var tradeProcessor = new TradeProcessor();
+            tradeProcessor.ProcessTrades(tradeStream);
+
+            //Act
+            int countBefore = CountDbRecords();
+            tradeProcessor.ProcessTrades(tradeStream);
+
+            //Assert - compare counts
+            int countAfter = CountDbRecords();
+            Assert.AreEqual(countBefore + 4, countAfter);
+
+           
+        }
+
+
+        private int CountDbRecords()
+        {
+            using (var connection = new System.Data.SqlClient.SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\azenk\source\repos\cis-3285-asg-8-azenk1\tradedatabase.mdf;Integrated Security=True;Connect Timeout=30;"))
+            {
+                connection.Open();
+                string myScalarQuery = "SELECT COUNT(*) FROM trade";
+                SqlCommand myCommand = new SqlCommand(myScalarQuery, connection);
+                myCommand.Connection.Open();
+                int count = (int)myCommand.ExecuteScalar();
+                connection.Close();
+                return count;
+            }
+        }
+    }
+}
